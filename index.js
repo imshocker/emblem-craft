9df83ecfb098a015759e974486ba65373766c7d9
Shocker <zacsmcdowell@gmail.com>
2023-11-06 14:53:22 -0500

    added generateShape & Shape child classes

--- index.js
+++ index.js
@@ -27,5 +27,44 @@ const getTextInput = async () => {
         type: 'input',
         name: 'text',
         message: 'Enter the text for the shape:',
-    });
+    },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the color (in CSS format) for the text:',
+        });
 };
+
+const generateShape = async () => {
+    try {
+        const shapeType = await getShapeInput();
+        const color = await getColorInput();
+        const textProp = await getTextInput();
+
+        let shape;
+
+        switch (shapeType.shape) {
+            case 'Triangle':
+                shape = new Triangle(color.color, textProp.text, textProp.textColor); // Pass text to the Triangle constructor
+                break;
+            case 'Circle':
+                shape = new Circle(color.color, textProp.text, textProp.textColor); // Pass text to the Circle constructor
+                break;
+            case 'Square':
+                shape = new Square(color.color, textProp.text, textProp.textColor); // Pass text to the Square constructor
+                break;
+            default:
+                console.log('Invalid shape selection');
+                return;
+        }
+        const svgContent = shape.render();
+        const fileName = `examples/${shapeType.shape.toLowerCase()}_logo.svg`;
+
+        fs.writeFileSync(fileName, svgContent);
+        console.log(`Generated ${fileName}`);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+};
+
+generateShape();
